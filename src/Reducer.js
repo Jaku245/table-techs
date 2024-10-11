5332de99486cc8d807ebfec7225d6b28d60ed2ab
Jaimin Desai <desaijaimin5@gmail.com>
2024-10-11 04:06:51 -0400

    Added Tray UI

--- src/Reducer.js
+++ src/Reducer.js
@@ -8,13 +8,18 @@ const Reducer = (state, action) => {
         case 'update_tray':
             return {
                 ...state,
-                tray: action.payload.tray
+                tray: {...action.payload.tray}
             };
         case 'select_item':
             return {
                 ...state,
-                tray: action.payload.tray,
-                selectedItem: action.payload.selectedItem
+                tray: {...action.payload.tray},
+                selectedItem: {...action.payload.selectedItem}
+            };
+        case 'update_table':
+            return {
+                ...state,
+                table: {...action.payload.table}
             };
         default:
             return {
